678aac25fa8c75b9358b8d16f52fb6e4f6ed85b1
Tajima <tajima@stylerecipe.co.jp>
2016-06-19 13:43:56 +0900

    colect some words

--- colect_related_word.py
+++ colect_related_word.py
@@ -23,34 +23,41 @@ def get_related_word(word):
     url = 'https://ja.wikipedia.org/w/api.php?format=json&action=query&list=backlinks&bltitle=%s' % encoded_word
     get_data = urllib.request.urlopen(url)
     json_data = json.loads(get_data.read().decode('utf-8'))
-    for i in json_data['query']['backlinks']:
-        if i['ns'] == 0:
-            related_word_list.append(i['title'])
+    if json_data.get('query', None) != None:
+        for data in json_data['query']['backlinks']:
+            if data['ns'] == 0:
+                related_word_list.append(data['title'])
     related_data[word] = related_word_list
     return related_data
 
 
 # json形式で'word: [related_words]'の形で収集
 if __name__ == '__main__':
+    count = 0 #収集した数
     with open("temp.csv", "w") as fout:
         fout.write("abc,def\nghi\x00,klm\n123,456")
 
     try:
-        related_word_file = open('related_word.json', 'w')
+        related_word_file = open('related_word2.json', 'w')
         related_word_file.write("{\"related\": [\n")
 
         with open(sys.argv[1]) as fin:
             reader = unfussy_reader(csv.reader(fin))
+            for row in reader:
+                if(row[0][0] == "お"):
+                    break
             for n, row in enumerate(reader):
-                keyword = row[0]
-                related_data = get_related_word(keyword)
-                print(related_data[keyword])
-                if len(related_data[keyword]) != 0:
-                    json.dump(get_related_word(row[0]), related_word_file, ensure_ascii=False)
-                    related_word_file.write(",\n")
-                    related_word_file.flush()
-    except:
-        print("エラー")
+                if(row[4] == "名詞"):
+                    keyword = row[0]
+                    related_data = get_related_word(keyword)
+                    print("%s:  %d" % (keyword, count))
+                    if len(related_data[keyword]) != 0:
+                        count += 1
+                        json.dump(get_related_word(row[0]), related_word_file, ensure_ascii=False)
+                        related_word_file.write(",\n")
+                        related_word_file.flush()
+    except Exception as e:
+        print(str(e))
     else:
         related_word_file.write("]}\n")
         related_word_file.close()
